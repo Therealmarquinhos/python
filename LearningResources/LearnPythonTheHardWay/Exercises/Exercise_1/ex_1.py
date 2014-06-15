fb81a76f6a9f2e456b7c4b94cabf2a51e5ca6915
meads58 <meads109@gmail.com>
2014-06-15 16:03:50 +0100

    learn python ex 1,2,3 and 4

---
+++ LearningResources/LearnPythonTheHardWay/Exercises/Exercise_1/ex_1.py
@@ -0,0 +1,6 @@
+#exercise 1 printing
+
+print 'this is some text '
+print  "using double quites"
+print 'seeing what "this" does '
+print "will this do 'something' different"
---
+++ LearningResources/LearnPythonTheHardWay/Exercises/Exercise_2/ex2.py
@@ -0,0 +1,7 @@
+#comments. On a MAC this is the alt + 3 keys
+
+print 'this will print test'
+
+#comments start with the hash key # and don't print
+
+print 'this will print # because the # is inside a string'
