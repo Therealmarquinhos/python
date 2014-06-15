3f851e51741752be605df4807ac38238569118d5
meads58 <meads109@gmail.com>
2014-06-15 15:17:37 +0100

    week 8, 9 work files

--- Coursera/week8.2.py
+++ Coursera/week8.2.py
@@ -1,1 +1,27 @@
-‡week 8
+# 
+# 8.5 Open the file mbox-short.txt and read it line by line. When you find
+# a line that starts with 'From ' like the following line: From
+# stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008 You will parse the
+# From line using split() and print out the second word in the line (i.e.
+# the entire address of the person who sent the message). Then print out a
+# count at the end. Hint: make sure not to include the lines that start
+# with 'From:'.
+# 
+# You can download the sample data at
+# http://www.pythonlearn.com/code/mbox-short.txtweek 8
+
+fname = raw_input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+lst = list()
+lst2 = list()
+
+for line in fh:
+    line = line.rstrip()
+    lst.append(line.split())
+
+
+print lst[2]
+
--- Coursera/week9.py
+++ Coursera/week9.py
@@ -1,2 +1,52 @@
 #week 9 
 
+#9.4 Write a program to read through the mbox-short.txt and figure out who has the most
+#commits. The program looks for 'From ' lines and takes the second word of those lines
+#as the person who sent the mail. The program creates a Python dictionary that maps the
+#senders mail address to a count of the number of times they appear in the file. After
+#the dictionary is produced, the program reads through the dictionary using a maximum
+#loop to find the most prolific committer.
+
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+
+words = list()
+person = list()
+
+
+#creates a list of email addresses
+def fromList():
+    for line in handle:
+        words = line.split()
+    
+        if len(words) == 0 :
+            continue 
+        if words[0] == 'From' :
+            person.append(words[1])
+    return person
+    
+#creates a dictionary and counts the number of times someones name appears        
+def countFroms(lstToUse):
+    d = dict()
+    pers = lstToUse
+    for p in pers:
+        d[p] = d.get(p,0) + 1
+    return d
+   
+def prolificCommitter():
+    dict = countFroms(fromList())
+    bigName = None
+    bigCount = None
+    
+    for ff,cc in dict.items():
+        if bigCount is None or cc > bigCount:
+            bigName = ff
+            bigCount = cc
+    print bigName, bigCount
+    return
+
+prolificCommitter()
+
+        
